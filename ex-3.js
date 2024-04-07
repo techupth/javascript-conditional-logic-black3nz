c6651f48439b6a271b2fff915ec979d52254a582
black3nz <pawich.p@hotmail.com>
2024-04-07 13:51:53 +0700

    Assignment submission

--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,7 @@
 //Exercise 3
 // Ternary Operator
 let lightBulbStatus = "Off";
-
+lightBulbStatus === "On" ?
+console.log("Light bulb is On.") : 
+console.log("Please choose the correct input (On/Off)");
 //Start coding here
